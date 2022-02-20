9d470ba693ee5b57577f9eae2b37961ee7a3a197
LeonardoGraham <leonardostefangraham@gmail.com>
2022-02-20 16:00:04 -0500

    Initial commit

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,30 @@
+﻿public class LinkedList<T>
+{
+    private class Node
+    {
+        public T Value { get; }
+        public Node? Next { get; }
+
+        public Node(T value, Node? next)
+        {
+            Value = value;
+            Next = next;
+        }
+    }
+    
+    private Node? _list;
+
+    public void Add(T value)
+    {
+        var node = new Node(value, _list);
+        _list = node;
+    }
+
+    public void Print()
+    {
+        for (var node = _list; node != null; node = node.Next)
+        {
+            Console.WriteLine(node.Value);
+        }
+    }
+}
---
+++ DataStructures/LinkedListNode.cs
@@ -0,0 +1,13 @@
+﻿namespace DataStructures;
+
+public class LinkedListNode<T>
+{
+    public T Value { get; }
+    public LinkedListNode<T>? Next { get; }
+
+    public Node(T value, LinkedListNode<T>? next)
+    {
+        Value = value;
+        Next = next;
+    }
+}
